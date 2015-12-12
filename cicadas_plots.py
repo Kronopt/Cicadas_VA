c8dc1cfb2cc9a93ddf64e1b905b09bb57f570736
LLCampos <luis1212@sapo.pt>
2015-12-12 16:31:27 +0000

    create script that builds plots from BehaviourSpace output

---
+++ cicadas_plots.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+data = open('Simulation_Data\cicadas.csv', 'r').readlines()
+
+data = data[15][2:-1].split('","')
+
+for i in range(0, len(data)):
+    plot_data = data[i][2:-2].split()
+
+    if i == len(data) - 1:
+        plot_data[-1] = plot_data[-1].replace(']', '')
+
+    plot_data = map(int, plot_data)
+
+    nbins = max(plot_data)
+
+    plt.clf()
+    plt.hist(plot_data, bins=nbins)
+    plt.xlabel('Lifecycle duration (years)')
+    plt.ylabel('Number of Cicadas')
+    plt.savefig('Plots\\test' + str(i + 1) + '.png')
