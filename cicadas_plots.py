6b719b8c88af865a8e20b53731b17267c6e4499e
LLCampos <luis1212@sapo.pt>
2015-12-13 11:17:22 +0000

    add xticks

--- cicadas_plots.py
+++ cicadas_plots.py
@@ -12,10 +12,14 @@ for i in range(0, len(data)):
 
     plot_data = map(int, plot_data)
 
-    nbins = max(plot_data)
+    maxduration = max(plot_data)
+    minduration = min(plot_data)
 
     plt.clf()
-    plt.hist(plot_data, bins=nbins)
+    plt.hist(plot_data, bins=maxduration)
     plt.xlabel('Lifecycle duration (years)')
     plt.ylabel('Number of Cicadas')
+    plt.xticks(range(minduration, maxduration + 1))
     plt.savefig('Plots\\test' + str(i + 1) + '.png')
+
+data.close()
