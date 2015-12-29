a44a0265fb8e8d5dffc1eab5b7e8b8378f71dd69
LLCampos <luis1212@sapo.pt>
2015-12-29 16:46:19 +0000

    adds winner cycle in the subtitle

--- cicadas_plots.py
+++ cicadas_plots.py
@@ -5,49 +5,50 @@ import matplotlib.pyplot as plt
 
 
 # dataf = open(filename, 'r')
-dataf = open('Hybrid Theory\Results\Experiment 7 - 12\Spreadsheet\cicadas-hybrid Experiment 12.csv', 'r')
-data = dataf.readlines()
 
-data = data[17][2:-1].split('","')
+for c in range(13, 22):
+    dataf = open('Hybrid Theory\Results\Experiments 13 - 15\Spreadsheets\cicadas-hybrid Experiment ' + str(c) + '.csv', 'r')
+    data = dataf.readlines()
 
-result_hist = []
+    data = data[17][2:-1].split('","')
 
-for i in range(0, len(data)):
-    plot_data = data[i][2:-2].split()
+    result_hist = []
 
-    if i == len(data) - 1:
-        plot_data[-1] = plot_data[-1].replace(']', '')
+    for i in range(0, len(data)):
+        plot_data = data[i][2:-2].split()
 
-    plot_data = map(int, plot_data)
+        if i == len(data) - 1:
+            plot_data[-1] = plot_data[-1].replace(']', '')
 
-    try:
-        maxduration = max(plot_data)
-        minduration = min(plot_data)
+        plot_data = map(int, plot_data)
 
-        plt.clf()
-        plt.hist(plot_data, bins=maxduration)
-        plt.xlabel('Lifecycle duration (years)')
-        plt.ylabel('Number of Cicadas')
-        plt.xticks(range(minduration, maxduration + 1))
-        plt.savefig('Hybrid Theory\Results\Experiment 7 - 12\Plots\Experiment 12 - ' + str(i + 1) + '.png')
-        result_hist.append(plot_data)
-    except ValueError:
-        plt.clf()
-        plt.savefig('Hybrid Theory\Results\Experiment 7 - 12\Plots\Experiment 12 - ' + str(i + 1) + '.png')
+        try:
+            maxduration = max(plot_data)
+            minduration = min(plot_data)
 
+            plt.clf()
+            plt.hist(plot_data, bins=maxduration)
+            plt.xlabel('Lifecycle duration (years)')
+            plt.ylabel('Number of Cicadas')
+            plt.xticks(range(minduration, maxduration + 1))
+            plt.savefig('Hybrid Theory\Results\Experiments 13 - 15\Plots\Experiment ' + str(c) + ' - ' + str(i + 1) + '.png')
+            result_hist.append(plot_data)
+        except ValueError:
+            plt.clf()
+            plt.savefig('Hybrid Theory\Results\Experiments 13 - 15\Plots\Experiment ' + str(c) + ' - ' + str(i + 1) + '.png')
 
-result_hist = map((lambda x: list(set(x))), result_hist)
-result_hist = [x[0] for x in result_hist if len(x) == 1]
+    result_hist = map((lambda x: list(set(x))), result_hist)
+    result_hist = [x[0] for x in result_hist if len(x) == 1]
 
-n = len(result_hist)
+    n = len(result_hist)
+    m = max(set(result_hist), key=result_hist.count)
 
-plt.clf()
-plt.hist(result_hist)
-plt.title('Experiment 12')
-plt.suptitle('n =' + str(n), fontsize=12)
-plt.xlabel('Lifecycle duration (years)')
-plt.ylabel('Frequency')
-plt.savefig('Hybrid Theory\Results\Experiment 7 - 12\Plots\Experiment 12.png')
+    plt.clf()
+    plt.hist(result_hist)
+    plt.title('Experiment ' + str(c))
+    plt.suptitle('n =' + str(n) + ' Winner =' + str(m), fontsize=12)
+    plt.xlabel('Lifecycle duration (years)')
+    plt.ylabel('Frequency')
+    plt.savefig('Hybrid Theory\Results\Experiments 13 - 15\Plots\Experiment ' + str(c) + '.png')
 
-
-dataf.close()
+    dataf.close()
